8697e4c2bc860ce72200cdd5f49992b64840ada4
Zheng Wang <zwang1208@gmail.com>
2019-05-07 12:09:32 -0400

    login/register page

--- src/container/login/login.js
+++ src/container/login/login.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react'
+import {List, InputItem, WingBlank, WhiteSpace, Button} from 'antd-mobile'
+
+import Logo from '../../component/logo/logo'
+import UserName from '../../static/img/yonghuming.png'
+import Password from '../../static/img/mima.png'
 
 class Login extends Component {
+    register = () => {
+        this.props.history.push('/register')
+    }
     render() {
-        return <h1>login</h1>
+        return (
+            <div>
+                <Logo></Logo>
+                <WingBlank>
+                    <List>
+                        <InputItem
+                            placeholder = 'Username'
+                        >
+                            <div style={{ backgroundImage: `url(${UserName})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </InputItem>
+                        <InputItem
+                            placeholder = 'Password'
+                        >
+                            <div style={{ backgroundImage: `url(${Password})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </InputItem>
+                    </List>
+                </WingBlank>
+                <WhiteSpace />
+                <WingBlank>
+                    <Button type='primary'>Sign in</Button>
+                    <WhiteSpace />
+                    <Button onClick={this.register} type='primary'>Sign up</Button>
+                </WingBlank>
+            </div>
+        )
     }
 }
 
--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,8 +1,57 @@
 import React, { Component } from 'react'
+import {List, InputItem, WingBlank, WhiteSpace, Button, Radio} from 'antd-mobile'
+
+import Logo from '../../component/logo/logo'
+import UserName from '../../static/img/yonghuming.png'
+import Password from '../../static/img/mima.png'
+import Client from '../../static/img/kehuguanli.png'
+import Service from '../../static/img/kefu.png'
+
+const RadioItem = Radio.RadioItem;
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: 'Client'
+        }
+    }
     render() {
-        return <h1>register</h1>
+        return (
+            <div>
+                <Logo></Logo>
+                <WingBlank>
+                    <List>
+                        <InputItem
+                            placeholder = 'Username'
+                        >
+                            <div style={{ backgroundImage: `url(${UserName})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </InputItem>
+                        <InputItem
+                            placeholder = 'Password'
+                        >
+                            <div style={{ backgroundImage: `url(${Password})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </InputItem>
+                        <InputItem
+                            placeholder = 'Confirm Password'
+                        >
+                            <div style={{ backgroundImage: `url(${Password})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </InputItem>
+                        <RadioItem
+                            checked={this.state.type === 'Client'}
+                        >
+                            <div style={{ backgroundImage: `url(${Client})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </RadioItem>
+                        <RadioItem
+                            checked={this.state.type === 'Service'}
+                        >
+                            <div style={{ backgroundImage: `url(${Service})`, backgroundSize: 'cover', height: '22px', width: '22px' }} />
+                        </RadioItem>
+                    </List>
+                </WingBlank>
+
+            </div>
+        )
     }
 }
 
