87df4e9532cf41f41b59d0324988f1f0ef706bbf
Zheng Wang <zwang1208@gmail.com>
2019-05-09 23:29:42 -0400

    connect mongodb

--- server/model.js
+++ server/model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const DB_URL = 'mongodb://localhost:27017/onlinechat'
 
-mongoose.connect(DB_URL)
+mongoose.connect(DB_URL,{ useNewUrlParser: true })
 mongoose.connection.on('connected', function(){
     console.log('mongodb connect successful')
 })
