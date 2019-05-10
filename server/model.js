f8391eda5019248cb231af866acb9b96517bbc8e
Zheng Wang <zwang1208@gmail.com>
2019-05-09 23:06:22 -0400

    add models

--- server/model.js
+++ server/model.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose')
 
-const DB_URL = 'mongodb://localhost:27017'
+const DB_URL = 'mongodb://localhost:27017/onlinechat'
 
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function(){
     console.log('mongodb connect successful')
 })
+
+const models = {
+    user: {
+        'username': {type: String, require: true},
+        'pwd': {type: String, require: true},
+        'type' : {type: String, require: true},
+        'avatar' : {type: String},
+        'desc' : {type: String},
+        'title' : {type: String}
+    },
+    chat: {
+
+    }
+}
+
+for(let i in models) {
+    mongoose.model(i, new mongoose.Schema(models[i]))
+}
+
+module.exports = {
+    getModel: function(name) {
+        return mongoose.model(name)
+    }
+}
